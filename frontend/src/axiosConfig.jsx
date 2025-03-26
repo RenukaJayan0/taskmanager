0edf2336dc61525e02168075f1790c38cc481e1f
RenukaJayan <n12134589@qut.edu.au>
2025-03-26 16:36:52 +1000

    fourth commited

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://54.206.62.12:5001', // local
-  //baseURL: 'http://54.206.62.12:5001', // live
+  //baseURL: 'http://54.206.62.12:5001', // local
+  baseURL: 'http://54.206.62.12:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
