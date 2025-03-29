47e5b7724012b135b628361037ac77c43b13b6db
RenukaJayan <n12134589@qut.edu.au>
2025-03-30 01:23:25 +1000

    changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'http://54.206.62.12:5000' });
 
 export const register = (data) => API.post('/auth/register', data);
 export const login = (data) => API.post('/auth/login', data);
